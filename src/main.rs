308d45f567e60737b82e59a03689d20d54238021
Mark Kevin Besinga <markkevin.besinga@hcl.com>
2024-02-29 08:39:54 +0800

    working dxclient rust wrapper

---
+++ src/main.rs
@@ -0,0 +1,137 @@
+use std::{env, fs};
+use std::io::{BufReader, BufRead};
+use std::path::Path;
+use std::process::{Command, Stdio};
+use atty::Stream;
+
+fn main() {
+    let image_name = "dxclient";
+    let image_tag = "local";
+
+    // parse command-line arguments
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    let container_folder = "/dxclient/store";
+    let args: Vec<String> = args
+        .iter()
+        .map(|arg| {
+            if Path::new(arg).exists() {
+                let base_name = Path::new(arg).file_name().unwrap().to_string_lossy();
+                return format!("{}/{}", container_folder, base_name)
+            }
+            arg.to_string()
+        }).collect();
+
+    //check for dependencies
+    // basically check if docker is registered properly on PATH
+    if let Err(err) = check_dependencies("docker") {
+        println!("{}", err);
+        return;
+    }
+
+    // handle environment variables
+    let volume_dir = env::var("VOLUME_DIR").unwrap_or_else(|_| "store".to_string());
+    let container_runtime = env::var("CONTAINER_RUNTIME").unwrap_or_else(|_| "docker".to_string());
+
+    // determine if running in a TTY
+    let tty_flag = match is_tty() {
+        true => "-t",
+        false => "",
+    };
+
+    // create volume directory if they don't exist
+    if let Err(err) = fs::create_dir_all(&volume_dir) {
+        println!("Error creating volume directory: {}", err);
+        return;
+    }
+
+    // generate volume parameters
+    let volume_params = format!(
+        "-v \"{}/{}:/dxclient/store\":Z",
+        env::current_dir().unwrap().to_string_lossy(),
+        volume_dir
+    );
+
+    // compose docker command
+    let docker_cmd = format!(
+        "{} run -e VOLUME_DIR=\"{}\" {} {} --network=host --platform linux/amd64 --name dxclient --rm {}:{} ./bin/dxclient {}",
+        container_runtime, volume_dir, tty_flag, volume_params, image_name, image_tag, args.join(" ")
+    );
+    println!("{}", docker_cmd);
+
+    // let output = Command::new("sh")
+    //     .arg("-c")
+    //     .arg(docker_cmd)
+    //     .output()
+    //     .expect("Failed to execute command");
+    //
+    // if !output.stdout.is_empty() {
+    //     println!("STDOUT {}", String::from_utf8_lossy(&output.stdout));
+    // }
+    // if !output.stderr.is_empty() {
+    //     println!("STDERR {}", String::from_utf8_lossy(&output.stderr));
+    // }
+    //
+    // if !output.status.success() {
+    //     println!("Error executing Docker command: {:?}", output.status);
+    //     return;
+    // }
+
+    let mut child = Command::new("sh")
+        .arg("-c")
+        .arg(docker_cmd)
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .expect("Failed to execute command");
+
+    // read and print stdout
+    let stdout = child.stdout.take().unwrap();
+    let stdout_reader = BufReader::new(stdout);
+    for line in stdout_reader.lines() {
+        println!("STDOUT: {}", line.expect("Failed to read STDOUT"));
+    }
+
+    // read and print stderr
+    let stderr = child.stderr.take().unwrap();
+    let stderr_reader = BufReader::new(stderr);
+    for line in stderr_reader.lines() {
+        println!("STDERR: {}", line.expect("Failed to read STDERR"));
+    }
+
+    // wait for command to finish
+    let status = child.wait().expect("Failed to wait for command");
+    if !status.success() {
+        println!("Error executing docker command: {:?}", status);
+        return;
+    }
+
+    cleanup_files(&args, &volume_dir);
+}
+fn check_dependencies(cmd: &str) -> Result<(), String> {
+    match Command::new("sh").arg("-c").arg(format!("{} -v", cmd)).output() {
+        Ok(output) => {
+            if output.status.success() {
+                Ok(())
+            } else {
+                Err(format!("{} command not found", cmd))
+            }
+        }
+        Err(err) => Err(format!("Error checking dependencies: {}", err)),
+    }
+}
+
+fn is_tty() -> bool {
+    atty::is(Stream::Stdout)
+}
+
+fn cleanup_files(args: &[String], volume_dir: &str) {
+    for arg in args {
+        if Path::new(arg).exists() {
+            let abs_path = Path::new(volume_dir).join(Path::new(arg).file_name().unwrap());
+            if let Err(err) = fs::remove_file(&abs_path) {
+                println!("Error removing file {}: {}", abs_path.display(), err);
+            }
+        }
+    }
+}
